7cba3d001e7ec079fbcaa979b86185fb2db5ded3
Yulia Glushenko <yuliaglushenko@Yulias-MacBook-Air.local>
2022-05-18 22:16:43 -0400

    things

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const mongoose = require ('mongoose');
 const app = express ();
 const db = mongoose.connection;
 require('dotenv').config()
-const session = require('express-session')
 
 //___________________
 //Port
@@ -50,20 +49,11 @@ app.use(express.json());// returns middleware that only parses JSON - may or may
 app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 
 
-app.use(
-  session({
-    secret: process.env.SECRET, //a random string do not copy this value or your stuff will get hacked
-    resave: false, // default more info: https://www.npmjs.com/package/express-session#resave
-    saveUninitialized: false // default  more info: https://www.npmjs.com/package/express-session#resave
-  })
-)
 
 
 const recipeController = require('./controllers/recipe.js');
 app.use(recipeController);
 
-const registerController = require('./controllers/register.js');
-app.use(registerController);
 
 
 //___________________
