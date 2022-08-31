24a327bbe4ee23f2deefc148d0463f733366cf51
Yulia Glushenko <yuliaglushenko@Yulias-MacBook-Air.local>
2022-08-31 15:51:09 -0400

    string

--- server.js
+++ server.js
@@ -25,14 +25,14 @@ const MONGODB_URI = process.env.MONGODB_URI;
 // Connect to Mongo &
 // Fix Depreciation Warnings from Mongoose
 // May or may not need these depending on your Mongoose version
-mongoose.connect(MONGODB_URI, ()=>{
+mongoose.connect('mongodb+srv://yuliaglushenko:Tears4Fears@cluster0.o7ier.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', ()=>{
     console.log('connected')
 }
 );
 
 // Error / success
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
-db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('connected', () => console.log('mongo connected: mongodb+srv://yuliaglushenko:Tears4Fears@cluster0.o7ier.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
 //___________________
@@ -68,4 +68,4 @@ app.use(recipeController);
 //___________________
 //Listener
 //___________________
-app.listen(PORT, () => console.log( 'Listening on port:', PORT))
+app.listen(3000, () => console.log( 'Listening on port:3000'))
